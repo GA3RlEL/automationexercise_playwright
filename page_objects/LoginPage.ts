0ff4c23c5da80bfb0a26ca3eacb252bc4d59839c
ga3riel <kruszkagabrys@gmail.com>
2025-07-18 23:40:26 +0200

    Added login test with valid credentials

--- page_objects/LoginPage.ts
+++ page_objects/LoginPage.ts
@@ -2,10 +2,15 @@ import { expect, Locator, Page } from "@playwright/test";
 
 export class LoginPage {
   private signUpForm: Locator;
+  private signInForm: Locator;
   private usernameRegisterInput: Locator;
   private emailRegisterInput: Locator;
   private newUserSignUpText: Locator;
   private signUpButton: Locator;
+  private loginToYourAccountText: Locator;
+  private emailLoginInput: Locator;
+  private passwordLoginInput: Locator;
+  private signInButton: Locator;
 
   constructor(private page: Page) {
     // Sign up form locators
@@ -16,11 +21,27 @@ export class LoginPage {
     this.signUpButton = this.signUpForm.getByRole("button");
 
     // Sign in form locators
+    this.signInForm = this.page.locator(".login-form");
+    this.loginToYourAccountText = this.signInForm.locator("h2");
+    this.emailLoginInput = this.signInForm.locator("input[type*='email']");
+    this.passwordLoginInput = this.signInForm.locator("input[type='password']");
+    this.signInButton = this.signInForm.getByRole("button", { name: "Login" });
   }
 
   async verifyNewUserSignUpTextIsVisible() {
     const isVisible = await this.newUserSignUpText.isVisible();
     expect(isVisible).toBeTruthy();
+  }
+
+  async verifyLoginToYourAccountTextIsVisible() {
+    const isVisible = await this.loginToYourAccountText.isVisible();
+    expect(isVisible).toBeTruthy();
+  }
+
+  async signIn(email: string, password: string) {
+    await this.emailLoginInput.fill(email);
+    await this.passwordLoginInput.fill(password);
+    await this.signInButton.click();
   }
 
   async signUp(email: string, name: string) {
---
+++ tests/ui/auth/login.spec.ts
@@ -0,0 +1,40 @@
+import { test } from "@playwright/test";
+import loginUser from "../../../data/loginUser.json";
+import { POManager } from "../../../page_objects/POManager";
+
+test.beforeEach(async ({ page }) => {
+  // Navigate to the home page
+  await page.goto("http://automationexercise.com/");
+  await page.waitForLoadState("networkidle");
+  // Automatically close window with consent to use your data
+  try {
+    await page.waitForSelector(".fc-cta-consent", { timeout: 5000 });
+    await page.locator(".fc-cta-consent").click();
+  } catch (error) {
+    throw new Error(
+      "Something went wrong while closing window with consent to use your data"
+    );
+  }
+});
+
+// In this test, deleting account is omitted
+test("Login user with valid credentials", async ({ page }) => {
+  const poManager = new POManager(page);
+  const homePage = poManager.getHomePage();
+  const loginPage = poManager.getLoginPage();
+
+  // Assert that the home page is displayed
+  await homePage.isAt();
+
+  // Navigate to the login page
+  await homePage.goToLoginPage();
+
+  // Assert that "Login to your account" text is visible
+  await loginPage.verifyLoginToYourAccountTextIsVisible();
+
+  // Sign in with valid credentials
+  await loginPage.signIn(loginUser.email, loginUser.password);
+
+  // Assert that the user is logged in
+  await homePage.verifyUserIsLoggedIn(loginUser.name);
+});
